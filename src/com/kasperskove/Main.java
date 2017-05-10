3e8debfba14d0a6085b815c6a97c7a7e7383cb09
kasper95 <kove_kasper@outlook.com>
2017-05-10 18:18:17 -0400

    coming back to this problem later

--- src/com/kasperskove/Main.java
+++ src/com/kasperskove/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// scratchpad... go crazy
+        System.out.printf(titleCase("Heelllooo world its me"));
+    }
+
+
+    public static String titleCase(String string){
+        // Write a headerliner(string) function that converts a string to title case (This Is Title Case).
+
+        // lowercase String so we only have to deal with first char per word
+        string.toLowerCase();
+        // how to get first char in each word
+        string.charAt(0);
+
+        //
+
+        return string;
     }
 }
