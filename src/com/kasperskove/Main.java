0667355e7bb211c53a1eac5a4da04cd2fb4ae4b5
kasper95 <kove_kasper@outlook.com>
2017-06-07 16:28:37 -0400

    +stringpractice directory

--- src/com/kasperskove/Main.java
+++ src/com/kasperskove/Main.java
@@ -1,23 +1,8 @@
 package com.kasperskove;
 
 public class Main {
+    // Scratchpad
 
     public static void main(String[] args) {
-	// scratchpad... go crazy
-        System.out.printf(titleCase("Heelllooo world its me"));
-    }
-
-
-    public static String titleCase(String string){
-        // Write a headerliner(string) function that converts a string to title case (This Is Title Case).
-
-        // lowercase String so we only have to deal with first char per word
-        string.toLowerCase();
-        // how to get first char in each word
-        string.charAt(0);
-
-        //
-
-        return string;
     }
 }
---
+++ src/com/kasperskove/stringpractice/Strings.java
@@ -0,0 +1,15 @@
+package com.kasperskove.stringpractice;
+
+public class Strings {
+
+    public static boolean isPalindrome (String s) {
+
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
